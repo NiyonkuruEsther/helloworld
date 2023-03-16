f13bfa72b18e03b8f147a9e0f49ee63bc0dee993
estherniyo <estherniyonkuru123@gmail.com>
2023-03-16 11:19:33 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HelloWorld from "./component/HelloWorld";
+
+function App() {
+  return <HelloWorld />;
+}
+
+export default App;
---
+++ src/component/HelloWorld.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function HelloWorld() {
+  return (
+    <div className="w-64 h-64 bg-gray-300 flex justify-center items-center">
+      <p>Hello, World!</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
